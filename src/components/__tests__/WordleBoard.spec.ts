6c3061677573d8d34bf57bb6bea7d99b24e7cd6e
Daniel Eicher <codingoak@gmail.com>
2024-03-23 20:47:23 +0100

    refactor: extract variable

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -3,18 +3,20 @@ import WordleBoard from '../WordleBoard.vue'
 import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '../../settings'
 
 describe('WordleBoard', () => {
+  let wordOfTheDay = "TEST"
+  
   it('should show a victory message when the user makes a guess that matches the word of the day', async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TEST' } })
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
 
     const guessInput = wrapper.find("input[type=text]")
-    await guessInput.setValue("TEST")
+    await guessInput.setValue(wordOfTheDay)
     await guessInput.trigger("keydown.enter")
 
     expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
 
   it("should show a defeat message if the user makes a guess that is incorrect", async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TEST' } })
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
 
     const guessInput = wrapper.find("input[type=text]")
     await guessInput.setValue("WRONG")
