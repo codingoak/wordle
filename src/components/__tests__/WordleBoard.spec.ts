997fdb6fabdff0e33e81905ca9d8823f746e82be
Daniel Eicher <codingoak@gmail.com>
2024-03-23 20:44:57 +0100

    refactor: extract constant

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
-import { VICTORY_MESSAGE } from '../../settings'
+import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '../../settings'
 
 describe('WordleBoard', () => {
   it('should show a victory message when the user makes a guess that matches the word of the day', async () => {
@@ -20,7 +20,7 @@ describe('WordleBoard', () => {
     await guessInput.setValue("WRONG")
     await guessInput.trigger("keydown.enter")
 
-    expect(wrapper.text()).toContain("Better luck next time!")
+    expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
   })
 
   it.todo("should not show an end-of-game message if the user has not made yet a guess")
