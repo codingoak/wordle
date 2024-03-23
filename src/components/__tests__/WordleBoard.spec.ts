4dca65eaa3cc1eee98c83ff4787d7390e2038299
Daniel Eicher <codingoak@gmail.com>
2024-03-23 20:04:02 +0100

    feat: render victory message

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,11 +1,17 @@
-// import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
 
 describe('WordleBoard', () => {
-  it('renders properly', () => {
-    const wrapper = mount(WordleBoard, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+  it('should show a victory message when the user makes a guess that matches the word of the day', async () => {
+    // Arrange
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TEST' } })
+
+    // Act
+    const guessInput = wrapper.find("input[type=text]")
+    await guessInput.setValue("TEST")
+    await guessInput.trigger("keydown.enter")
+
+    // Assert
+    expect(wrapper.text()).toContain("You won!")
   })
 })
