ee2ef3498a482836952942f366a7aca0ed79b15e
Daniel Eicher <codingoak@gmail.com>
2024-03-23 20:05:56 +0100

    refactor: remove clutter

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -3,15 +3,12 @@ import WordleBoard from '../WordleBoard.vue'
 
 describe('WordleBoard', () => {
   it('should show a victory message when the user makes a guess that matches the word of the day', async () => {
-    // Arrange
     const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TEST' } })
 
-    // Act
     const guessInput = wrapper.find("input[type=text]")
     await guessInput.setValue("TEST")
     await guessInput.trigger("keydown.enter")
 
-    // Assert
     expect(wrapper.text()).toContain("You won!")
   })
 })
