651fca488f26d5d97aaf39f1aa09f83639e71451
Daniel Eicher <codingoak@gmail.com>
2024-03-23 20:40:59 +0100

    feat: display defeat message

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -12,4 +12,16 @@ describe('WordleBoard', () => {
 
     expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
+
+  it("should show a defeat message if the user makes a guess that is incorrect", async () => {
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TEST' } })
+
+    const guessInput = wrapper.find("input[type=text]")
+    await guessInput.setValue("WRONG")
+    await guessInput.trigger("keydown.enter")
+
+    expect(wrapper.text()).toContain("Better luck next time!")
+  })
+
+  it.todo("should not show an end-of-game message if the user has not made yet a guess")
 })
